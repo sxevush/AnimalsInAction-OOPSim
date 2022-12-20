03de17af37e655de348aa425bb15bbea252d24b3
Ksawery Ostrowski <411530@student.agh.edu.pl>
2022-12-20 23:42:45 +0100

    test

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -1,7 +1,6 @@
 package agh.ics.oop;
 import java.util.ArrayList;
 import java.util.Arrays;
-import static java.lang.Math.random;
 import java.util.List;
 
 public class Animal {
--- src/main/java/agh/ics/oop/Vector2d.java
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -1,50 +1,37 @@
 package agh.ics.oop;
-import java.util.Objects;
 
-public class Vector2d {
-    public final int x;
-    public final int y;
+public record Vector2d(int x, int y) {
 
-    public Vector2d(int x, int y){
-        this.x = x;
-        this.y = y;
+    public String toString() {
+        return "(%d, %d)".formatted(x, y);
     }
 
-    public String toString(){
-        return "(%d, %d)".formatted(x,y);
+    public boolean precedes(Vector2d other) {
+        return other.x >= this.x && other.y >= this.y;
+    }
 
+    public boolean follows(Vector2d other) {
+        return other.x <= this.x && other.y <= this.y;
     }
-    public boolean precedes(Vector2d other){
-        if(other.x>=this.x && other.y>=this.y){
-            return true;
 
-        }
-        return false;
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(this.x + other.x, this.y + other.y);
     }
-    public boolean follows(Vector2d other){
-        if(other.x<=this.x && other.y<=this.y){
-            return true;
 
-        }
-        return false;
+    public Vector2d subtract(Vector2d other) {
+        return new Vector2d(this.x - other.x, this.y - other.y);
     }
-    public Vector2d add(Vector2d other){
-        Vector2d v2 = new Vector2d(this.x + other.x, this.y + other.y);
-        return v2;
-    }
-    public Vector2d subtract(Vector2d other){
-        Vector2d v2 = new Vector2d(this.x - other.x, this.y - other.y);
-        return v2;
-    }
-    public Vector2d upperRight(Vector2d other){
+
+    public Vector2d upperRight(Vector2d other) {
         return new Vector2d(Math.max(this.x, other.x), Math.max(this.y, other.y));
     }
 
-    public Vector2d lowerLeft(Vector2d other){
+    public Vector2d lowerLeft(Vector2d other) {
         return new Vector2d(Math.min(this.x, other.x), Math.min(this.y, other.y));
     }
-    public Vector2d opposite(){
-        return new Vector2d(-this.x,-this.y);
+
+    public Vector2d opposite() {
+        return new Vector2d(-this.x, -this.y);
     }
 
 
@@ -55,20 +42,14 @@ public class Vector2d {
         return x == vector2d.x && y == vector2d.y;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
     public static void main(String[] args) {
-        Vector2d position1 = new Vector2d(1,2);
+        Vector2d position1 = new Vector2d(1, 2);
         System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
+        Vector2d position2 = new Vector2d(-2, 1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
 
     }
-
 
 
 }
